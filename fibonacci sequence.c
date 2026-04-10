b82b9ec8cb10c36d4ba792e314c854f0be426d34
Mohammed shakib <5645mohammedshakib@gmail.com>
2026-04-10 23:47:13 +0530

    Refactor Fibonacci sequence program formatting

--- fibonacci sequence.c
+++ fibonacci sequence.c
@@ -2,16 +2,16 @@
 int main(void)
 {
 	int i,n;
-	//initialize first and second terms
+	
 	int t1=0,t2=1;
-	//initialize the next term (3rd term)
+	
 	int nextTerm=t1+t2;
-	//get no. of terms from user 
+	
 	printf("enter the number of terms:");
 	scanf("%d",&n);
-	//print the first two terms t1 and t2
+	
 	printf("fibonacci series :%d,%d,",t1,t2);
-	//print 3rd to nth term
+	
 	for(i=3;i<=n;++i)
 	{
 		printf("%d,",nextTerm);
